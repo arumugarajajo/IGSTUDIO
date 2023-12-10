cc95e707905ac54323cead807ca0d2405ca0485c
arumugarajajo <arumugarajajo@gmail.com>
2023-12-10 23:54:29 +0530

    carousel completed

--- src/components/NavbarWithProfileContainer/NavbarWithProfileContainer.jsx
+++ src/components/NavbarWithProfileContainer/NavbarWithProfileContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import style from "./NavbarWithProfileContainer.module.css";
 import logo from "../../Assets/logo.png";
 import message from "../../Assets/message.png";
@@ -6,11 +6,6 @@ import Button from "../../components/Button/Button";
 import profile from "../../Assets/profile.png";
 import bg from "../../Assets/bg.png";
 function NavbarWithProfileContainer() {
-  const [showList, setShowList] = React.useState(false);
-
-  const toggleList = () => {
-    setShowList(!showList);
-  };
   return (
     <div className={style.wrapper}>
       <div className={style.logoNavbar}>
@@ -19,17 +14,11 @@ function NavbarWithProfileContainer() {
           <span>IGSTUDIO</span>
         </div>
         <div className={style.navbarLi}>
-          <ul onClick={toggleList}>
-            <li>
-              <i className="fas fa-home"></i>
-            </li>
-            {showList && (
-              <>
-                <li>Attorneys</li>
-                <li>Practice Areas</li>
-                <li>About Us</li>
-              </>
-            )}
+          <ul>
+            <li>Home</li>
+            <li>Attorneys</li>
+            <li>Practice Areas</li>
+            <li>About Us</li>
           </ul>
         </div>
         <div className={style.navtop}>
@@ -55,7 +44,7 @@ function NavbarWithProfileContainer() {
           </div>
         </div>
         <div className={style.rightPortion}>
-          <div className={style.rightprofileContainer}>
+        <div className={style.rightprofileContainer}>
             <img src={profile} alt="profile" />
           </div>
         </div>
